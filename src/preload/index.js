a6c6d12d7ff1397f17ba3dfbd6fc5a6c50574a9e
Kosaka Bun <kosaka-bun@qq.com>
2023-09-29 22:44:12 +0800

    Reformat comments

--- src/preload/index.js
+++ src/preload/index.js
@@ -1,12 +1,12 @@
 import { contextBridge } from 'electron'
 import { electronAPI } from '@electron-toolkit/preload'
 
-// Custom APIs for renderer
+//Custom APIs for renderer
 const api = {}
 
-// Use `contextBridge` APIs to expose Electron APIs to
-// renderer only if context isolation is enabled, otherwise
-// just add to the DOM global.
+//Use `contextBridge` APIs to expose Electron APIs to
+//renderer only if context isolation is enabled, otherwise
+//just add to the DOM global.
 if(process.contextIsolated) {
   try {
     contextBridge.exposeInMainWorld('electron', electronAPI)
